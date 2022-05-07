edf750a0a717d37cba38b491b3fb78bce4a072a6
Thomas Luechtefeld <tom@insilica.co>
2022-05-07 18:10:24 -0400

    simplified initialize

--- R/initialize.R
+++ R/initialize.R
@@ -1,8 +1,7 @@
 #' create the git and dvc repos for the biobricks library
 #' @export
 initialize <- function(){
-  has_git <- fs::path(bblib(),".git") |> fs::file_exists()
-  if(!has_git){ system(sprintf("git init %s",bblib())) }
-  
+  if(fs::file_exists(bblib(".git"))){ return("already init") }
+  system(sprintf("git init %s",bblib()))
   sprintf("initialized biobricks to %s\n",bdir) |> cat()
 }
