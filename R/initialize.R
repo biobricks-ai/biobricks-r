fb3d1573e3e9402d61e16c26f246ef7fcca8957c
Thomas Luechtefeld <tom@insilica.co>
2022-05-07 16:14:46 -0400

    removed git config

--- R/initialize.R
+++ R/initialize.R
@@ -1,10 +1,8 @@
 #' create the git and dvc repos for the biobricks library
 #' @export
 initialize <- function(){
-  bdir <- bblib()
-  if(!file.exists(fs::path(bdir,".git"))){
-    system(sprintf("git init %s",bblib()))
-  }
-  set_git_config()
-  cat(sprintf("initialized biobricks to %s\n",bdir))
+  has_git <- fs::path(bblib(),".git") |> fs::file_exists()
+  if(!has_git){ system(sprintf("git init %s",bblib())) }
+  
+  sprintf("initialized biobricks to %s\n",bdir) |> cat()
 }
