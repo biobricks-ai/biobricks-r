dafd59b23f66bfba6c81b0965a93614754143cea
Thomas Luechtefeld <tom@insilica.co>
2022-05-07 09:57:23 -0400

    resolve bricks and execute docker commands relative to bblib

--- R/bake.R
+++ R/bake.R
@@ -17,27 +17,20 @@ update_brick <- function(brick){
 #' biobricks::bake("clinvar")
 #' }
 bake <- function(brick){
-  bdir   <- resolve(brick)
-  brel   <- fs::path(bdir) |> fs::path_rel(bblib())
-  result <- docker_run(
-    "dvc repro",
-    wd = sprintf("/biobricks/bricks/%s", brel))
-
-  # if (system(sprintf("cd %s ; git diff --exit-code",bblib())) != 0) {
-  #   warning("Warning: Changes to the repo should not happen!")
-  # }
+  bdir   <- resolve(brick,rel=bblib())
+  result <- docker_run("dvc repro",wd=bdir)
   fs::path(bdir,"data") |> fs::dir_ls(recurse=TRUE)
 }
 
 #' Finds all the bricks in `bblib()` matching @param brick
 #' @param brick the brick you want to find 
+#' @param rel resolve relative to what path
 #' @export
 #' @example
 #' \dontrun{
 #' biobricks::resolve("clinvar")
 #' }
-resolve <- function(brick){
-  bd   <- bblib()
-  dirs <- fs::dir_ls(bd,recurse=T,regexp=fs::path(brick,"dvc.yaml"))
-  fs::path_dir(dirs)
+resolve <- function(brick,rel="/"){
+  dirs <- fs::dir_ls(bblib(),recurse=T,regexp=fs::path(brick,"dvc.yaml"))
+  fs::path_dir(dirs) |> fs::path_rel(start=rel)
 }
--- R/docker.R
+++ R/docker.R
@@ -1,10 +1,10 @@
 #' Run a cmd in the docker container
 #' @param cmd The command to run
-#' @param wd Working directory context to execute cmd
+#' @param wd Working directory context (relative to bblib) to execute cmd
 #' @export
 docker_run <- function(cmd,wd=bblib()) {
     mnt    <- sprintf("-v %s:/biobricks/bricks",bblib())
-    wd     <- sprintf("-w %s",wd)
+    wd     <- sprintf("-w %s",fs::path("biobricks/bricks",wd))
     dkr    <- sprintf("docker run --rm %s %s insilica/biobricks:latest",mnt,wd)
     dcmd   <- sprintf("%s %s",dkr,cmd)
     system(dcmd)
