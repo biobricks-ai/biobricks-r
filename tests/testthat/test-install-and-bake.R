84bf0423806c1a9c0e67de879391bb8d7a9fce50
Thomas Luechtefeld <tom@insilica.co>
2022-05-09 12:52:42 -0400

    update docs on tests

--- tests/testthat/test-install-and-bake.R
+++ tests/testthat/test-install-and-bake.R
@@ -6,10 +6,11 @@ test_that("install-and-bake-system", {
   
   biobricks::install_gh("biobricks-ai/hello-brick")
   biobricks::bake("hello-brick", env="system")
+  biobricks::bricktables("hello-brick") # TODO add a table to hello-brick
 })
 
 test_that("install-and-bake-docker", {
-  # TODO make an skipped test for the docker env
+  # TODO #6 make an skipped test for the docker env
   # add fixtures (see https://testthat.r-lib.org/articles/test-fixtures.html) and belo
   # bblib <- withr::local_dir(tempdir(),getwd()) 
   # withr::with_envvar(new=c("bblib"=bblib),Sys.getenv("bblib"))
