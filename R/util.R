34135b8e4a07609438704b12bb268549b96c1017
Thomas Luechtefeld <tom@insilica.co>
2022-05-10 09:00:28 -0400

    add local_bblib to util.R

--- R/util.R
+++ R/util.R
@@ -2,3 +2,10 @@
 systemf <- function(...,intern=F){
   system(sprintf(...),intern=intern)
 }
+
+#' @keywords internal
+local_bblib <- function(env=parent.frame()){
+  bblib <- withr::local_tempdir(.local_envir = env)
+  withr::local_envvar(list(bblib=bblib),.local_envir = env)
+  biobricks::initialize()
+}
