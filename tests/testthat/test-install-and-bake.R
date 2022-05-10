742256aa0279b819af252d0e17d4d6b51873c491
Thomas Luechtefeld <tom@insilica.co>
2022-05-09 20:59:57 -0400

    fixes #5 and #6

--- tests/testthat/test-install-and-bake.R
+++ tests/testthat/test-install-and-bake.R
@@ -6,23 +6,26 @@ local_bblib <- function(env=parent.frame()){
 
 test_that("install-and-bake-system", {
   local_bblib()
-  install_gh("biobricks-ai/hello-brick")
-  bake("hello-brick", env="system")
+  biobricks::install_gh("biobricks-ai/hello-brick")
+  biobricks::bake("hello-brick", env="system")
   
   hellotbl <- bricktables("hello-brick")$mtcars |> dplyr::collect()
   rownames(mtcars) <- 1:32
   expect_equal(hellotbl,mtcars)
 })
 
-# test_that("install-and-bake-docker", {
-#   # TODO #6 make an skipped test for the docker env
-#   # add fixtures (see https://testthat.r-lib.org/articles/test-fixtures.html) and belo
-#   # bblib <- withr::local_dir(tempdir(),getwd()) 
-#   # withr::with_envvar(new=c("bblib"=bblib),Sys.getenv("bblib"))
-  
-#   biobricks::install_gh("biobricks-ai/hello-brick")
-#   biobricks::bake("hello-brick", env="docker")
-# })
+test_that("install-and-bake-docker", {
+  has_docker = suppressWarnings(system("docker"))
+  testthat::skip_if(has_docker!=0) # need docker for test
+
+  local_bblib() 
+  biobricks::install_gh("biobricks-ai/hello-brick")
+  biobricks::bake("hello-brick", env="docker")
+
+  hellotbl <- bricktables("hello-brick")$mtcars |> dplyr::collect()
+  rownames(mtcars) <- 1:32
+  expect_equal(hellotbl,mtcars)
+})
 
 # test_that("bricks-with-dependencies-work", {
 #   # TODO #8 make a test for bricks with dependencies
