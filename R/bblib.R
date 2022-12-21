3c1c3c20b0b7697da9a276a6983fff34a88bac7f
Thomas Luechtefeld <tom@insilica.co>
2022-12-21 09:17:41 -0500

    add varargs to bblib path resolution

--- R/bblib.R
+++ R/bblib.R
@@ -1,8 +1,9 @@
 #' get location for storing bricks
 #' @param path get path relative to bblib
+#' @param ... additional path components
 #' @export 
-bblib <- function(path=""){
+bblib <- function(path="",...){
   if(Sys.getenv("BBLIB")==""){ stop("No BBLIB. Use `Sys.setenv(BBLIB='...')`") }
   bblib = fs::dir_create(Sys.getenv("BBLIB"))
-  fs::path_real(bblib) |> fs::path(path)
+  fs::path_real(bblib) |> fs::path(path,...)
 }
