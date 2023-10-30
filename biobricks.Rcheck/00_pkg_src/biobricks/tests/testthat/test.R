dda93feff018ef2561aec15004fd7d3f6ea45cf3
Thomas Luechtefeld <tom@insilica.co>
2023-10-30 18:11:04 -0400

    remove files that should be ignored

--- biobricks.Rcheck/00_pkg_src/biobricks/tests/testthat/test.R
+++
@@ -1,5 +0,0 @@
-test_that("biobricks can load hello-brick", {
-  assets <- bbassets("hello-brick")
-  mynames <- c("rtbls.mtcars.parquet","rtbls.iris.parquet","iris.sqlite","mtcars.parquet")
-  expect_true(all(mynames %in% names(assets)))
-})
